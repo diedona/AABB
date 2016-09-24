0db96a4aca1052b8f46f7ab571a28f7b9e547892
diego <diego@DESKTOP-891SC3M>
2016-09-24 14:25:21 -0300

    futebol

---
+++ AABB.WebSite/Controllers/AtividadesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AABB.WebSite.Controllers
+{
+    public class AtividadesController : Controller
+    {
+        public ActionResult Futebol()
+        {
+            return View();
+        }
+    }
+}
