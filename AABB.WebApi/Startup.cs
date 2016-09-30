0203c4f0687931d3e1a40d1ccfaa21b6f57e3010
diego <diego@DESKTOP-891SC3M>
2016-09-29 22:49:55 -0300

    admin

---
+++ AABB.WebApi/Startup.cs
@@ -0,0 +1,20 @@
+﻿using AABB.WebApi.App_Start;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AABB.WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
